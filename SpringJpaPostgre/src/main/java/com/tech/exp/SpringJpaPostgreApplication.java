d775a2fd04c7945445557cc1271da094c355d5e3
AD <AD@192.168.1.3>
2019-08-02 17:21:35 +0530

    firtst

---
+++ SpringJpaPostgre/src/main/java/com/tech/exp/SpringJpaPostgreApplication.java
@@ -0,0 +1,12 @@
+package com.tech.exp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class SpringJpaPostgreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaPostgreApplication.class, args);
+	}
+
+}
